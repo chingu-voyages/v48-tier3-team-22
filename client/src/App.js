df4603a8e0cdd5caee993c1a3e5fed23ee8096ce
teodora-kocic <teodora.kocic1125@gmail.com>
2024-03-17 17:16:55 +0100

    feat(dinosaur-page)

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import styles from "./App.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import DinosaurPage from "./components/Dinosaurs/DinosaurPage";
+
+console.log(import.meta.env);
 
 function App() {
   return (
     <div className={styles.appContainer}>
       <Header />
+      <DinosaurPage />
       <Footer />
     </div>
   );
