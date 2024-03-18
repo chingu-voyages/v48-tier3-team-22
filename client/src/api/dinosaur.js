320d1581e5b7467fea4fba26854691c4109b1a28
WarrenHawker <hawker.warren@gmail.com>
2024-03-18 10:14:57 +0000

    removed old client folder

--- client/src/api/dinosaur.js
+++
@@ -1,8 +0,0 @@
-import http from "./api";
-
-const apiUrl =
-  import.meta?.env?.VITE_API_URL || `https://chinguapi.onrender.com`;
-
-export const getDinosaurs = (params = {}) => {
-  return http.get(`${apiUrl}/dinosaurs`, JSON.stringify(params));
-};
