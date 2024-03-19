d7e355de4a7ddd260d5a4404226db4eafb212a55
“vukas86” <vukasbit@gmail.com>
2024-03-19 16:16:52 +0100

    finish chart start with location of the Dinosaur

--- client/src/components/Chart/Chart.jsx
+++ client/src/components/Chart/Chart.jsx
@@ -1,8 +1,11 @@
 import { useState, useEffect } from "react";
 import { getDinosaurs } from "../../api/dinosaur";
+import { Chart as ChartJS } from "chart.js/auto";
+import { Bar } from "react-chartjs-2";
 
 function Chart() {
   const [dinoData, setDinoData] = useState([]);
+  const [dinoDiet, setDinoDiet] = useState({});
 
   useEffect(() => {
     getDinosaurs()
@@ -12,7 +15,42 @@ function Chart() {
       .catch((err) => console.log(err));
   }, []);
 
-  return <div>Some random chart</div>;
+  useEffect(() => {
+    const calcuclatedDiet = () => {
+      const counts = dinoData.reduce((acc, dino) => {
+        acc[dino.diet] = (acc[dino.diet] || 0) + 1;
+        return acc;
+      }, {});
+      setDinoDiet(counts);
+    };
+    calcuclatedDiet();
+  }, [dinoData]);
+
+  let herbivorous = dinoDiet["herbivorous"];
+  let carnivorous = dinoDiet["carnivorous"];
+  let omnivorous = dinoDiet["omnivorous"];
+
+  return (
+    <div className="chartContainer">
+      <Bar
+        data={{
+          labels: ["Herbivorous", "Carnivorous", "Omnivorous"],
+          datasets: [
+            {
+              label: "DINOSAUR DIET VARIABILITY",
+              data: [herbivorous, carnivorous, omnivorous],
+              backgroundColor: [
+                "rgba(43,63,229,0.8",
+                "rgba(250,192,19,0.8",
+                "rgba(253,135,0.8)",
+              ],
+              borderColor: ["rgba(0, 0, 0, 0.5);"],
+            },
+          ],
+        }}
+      />
+    </div>
+  );
 }
 
 export default Chart;
---
+++ client/src/components/Location/DinosaurLocation.jsx
@@ -0,0 +1,3 @@
+import { useState, useEffect } from "react";
+import Card from "react-bootstrap/Card";
+import { getDinosaurs
