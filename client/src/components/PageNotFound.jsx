40e479d9e226fc6fde844ab167729ee760b6af0b
“vukas86” <vukasbit@gmail.com>
2024-03-29 20:14:40 +0100

    add image for 404 page

--- client/src/components/PageNotFound.jsx
+++ client/src/components/PageNotFound.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import ntf from "../assets/404.png";
 
 function PageNotFound() {
-  return <div className="pt-[115px]">PageNotFound</div>;
+  return (
+    <div className="pt-[115px] m-8 content-center">
+      <h1>PAGE NOT FOUND</h1>
+      <div className="ntf-container">
+        <img src={ntf} alt="not found page" />
+      </div>
+    </div>
+  );
 }
 
 export default PageNotFound;
