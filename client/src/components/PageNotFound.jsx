2dfafdaa81510c157fb0851ed6faa1ef29d7bb22
“vukas86” <vukasbit@gmail.com>
2024-03-30 03:29:45 +0100

    adjust and center content

--- client/src/components/PageNotFound.jsx
+++ client/src/components/PageNotFound.jsx
@@ -2,10 +2,14 @@ import ntf from "../assets/404.png";
 
 function PageNotFound() {
   return (
-    <div className="pt-[115px] m-8 content-center">
-      <h1>PAGE NOT FOUND</h1>
-      <div className="ntf-container">
-        <img src={ntf} alt="not found page" />
+    <div className="pt-[115px] flex justify-center items-center h-screen">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold text-green-500 mb-6">
+          PAGE NOT FOUND
+        </h1>
+        <div className="ntf-container">
+          <img src={ntf} alt="not found page" />
+        </div>
       </div>
     </div>
   );
