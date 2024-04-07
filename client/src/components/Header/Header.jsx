483026d066b42a10022c59ba8b7382f71bb33c5f
teodora-kocic <teodora.kocic1125@gmail.com>
2024-04-07 18:40:47 +0200

    fix(ui): improving ui on header, dinosaur page and home page

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -7,27 +7,20 @@ import DinoLogo from "../../assets/dino-logo.png";
 
 import MenuIcon from "../../assets/menu-icon.png";
 import CloseMenuIcon from "../../assets/close-menu-icon.png";
+import UserIcon from "../../assets/user-logo.png";
+import LogoutIcon from "../../assets/logout.png";
 
 const Header = () => {
-  const [isClicked, setIsClicked] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isNameClicked, setIsNameClicked] = useState(false);
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const loggedIn = useSelector((state) => state.user.isLoggedIn);
   const userName = useSelector((state) => state.user.userName);
 
-  const toggleBtn = () => {
-    setIsClicked(!isClicked);
-  };
-
   const handleLogout = () => {
     dispatch(logoutAction());
     navigate("/");
-  };
-
-  const closeBtn = (route) => {
-    setIsClicked(false);
-    navigate(route);
   };
 
   const toggleMenu = () => {
@@ -77,60 +70,47 @@ const Header = () => {
       </nav>
 
       {loggedIn ? (
-        <div>
-          <p>Welcome, {userName}</p>
-          <p
-            className="p-[5px] hover:font-bold cursor-pointer"
-            onClick={handleLogout}
-          >
-            Log Out
-          </p>
-        </div>
-      ) : (
-        <div className="flex items-center">
-          <button
-            onClick={toggleBtn}
-            className="p-[8px] bg-emerald-500 text-[#fff] font-bold rounded-xl text-[14px] md:text-[20px] relative hidden md:block"
-          >
-            Get started
-          </button>
-          {isClicked && (
-            <div className="absolute flex flex-col justify-center items-center bg-[#fff] text-emerald-500 rounded-b-xl rounded-t top-[53px] md:right-[18px] md:top-[71px] w-[93px] md:w-[125px] p-[10px]">
-              <p
-                className="p-[5px] hover:font-bold cursor-pointer text-[14px] md:text-base"
-
-                onClick={() => closeBtn("/auth/register")}
-
-               
-
-              >
-                Sign Up
-              </p>
-              <p
-                className="p-[5px] hover:font-bold cursor-pointer text-[14px] md:text-base"
-
-                onClick={() => closeBtn("/auth/login")}
-
-               
-
-              >
-                Log In
-              </p>
-            </div>
-          )}
-
+        <div className="flex items-center gap-x-[10px] relative">
+          <p>Welcome,</p>
           <div
-            onClick={toggleMenu}
-            className="md:hidden w-[40px] ml-[10px] md:ml-[20px]"
+            className="font-bold cursor-pointer  text-[#fff]  md:text-[18px] flex items-center justify-center "
+            onClick={() => setIsNameClicked((name) => !name)}
           >
-            {isMenuOpen ? (
-              <img src={CloseMenuIcon} alt="" className="cursor-pointer" />
-            ) : (
-              <img src={MenuIcon} alt="" className="cursor-pointer" />
-            )}{" "}
+            {userName}
+
+            {isNameClicked && (
+              <div className=" bg-[#fff] absolute left-0 right-0 top-[40px] text-emerald-500 rounded-b-lg rounded-l-lg gap-y-[5px] flex flex-col  font-normal px-[3px] py-[15px] text-[16px]">
+                <div className="flex items-center gap-x-[5px] ml-[5px]">
+                  <img className="w-[20px]" src={UserIcon} alt="" />
+                  <p>{userName}</p>
+                </div>
+
+                <div
+                  className="flex items-center hover:font-bold cursor-pointer gap-x-[5px] ml-[5px]"
+                  onClick={handleLogout}
+                >
+                  <img className="w-[20px]" src={LogoutIcon} alt="" />
+                  <p>Log Out</p>
+                </div>
+              </div>
+            )}
           </div>
         </div>
+      ) : (
+        ""
       )}
+      <div className="flex items-center md:hidden">
+        <div
+          onClick={toggleMenu}
+          className="md:hidden w-[40px] ml-[10px] md:ml-[20px]"
+        >
+          {isMenuOpen ? (
+            <img src={CloseMenuIcon} alt="" className="cursor-pointer" />
+          ) : (
+            <img src={MenuIcon} alt="" className="cursor-pointer" />
+          )}{" "}
+        </div>
+      </div>
     </header>
   );
 };
