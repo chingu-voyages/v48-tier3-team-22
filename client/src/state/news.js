d9d3ea0724e23d281e8622fa79442cea6cf8a06d
WarrenHawker <hawker.warren@gmail.com>
2024-04-14 20:46:15 +0100

    changed news api url

--- client/src/state/news.js
+++ client/src/state/news.js
@@ -4,8 +4,8 @@ import axios from 'axios';
 export const fetchNews = createAsyncThunk('news/fetchNews', () => {
   return axios
     .get(
-      // `https://newsapi.org/v2/everything?qInTitle=dinosaurs&apiKey=82fe875a3ebf429badca4cb752ea227d`
-      'https://v48-tier3-team-22-api.onrender.com/api/news'
+      `https://newsapi.org/v2/everything?qInTitle=dinosaurs&apiKey=82fe875a3ebf429badca4cb752ea227d`
+      // 'https://v48-tier3-team-22-api.onrender.com/api/news'
     )
     .then((response) => response.data);
 });
