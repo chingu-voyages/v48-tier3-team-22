fa8ae409b628f4197ca3f3fd9654da420848697f
teodora-kocic <teodora.kocic1125@gmail.com>
2024-04-14 17:01:55 +0200

    improving header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -31,9 +31,7 @@ const Header = () => {
   };
 
   return (
-
-    <header className="p-[15px] md:p-[18px] flex flex-row justify-between items-center text-emerald-100   md:my-0 fixed z-100 top-0 left-0 right-0 bg-gradient-to-tl from-green-950 to-black-950">
-
+    <header className="p-[15px] md:p-[18px] flex flex-row justify-between items-center text-emerald-100   md:my-0 fixed z-50 top-0 left-0 right-0 bg-gradient-to-tl from-green-950 to-black-950">
       <div className="flex items-center">
         <img src={DinoLogo} alt="" className="w-[40px] md:w-[60px]" />
         <h1 className="ml-[10px] md:ml-[30px] md:text-[30px] font-bold">
@@ -70,45 +68,45 @@ const Header = () => {
         </NavLink>
       </nav>
       {loggedIn ? (
-
         <div className="flex items-center gap-x-[10px] relative">
           <p>Welcome,</p>
           <div
             className="font-bold cursor-pointer text-[#fff] md:text-[18px] flex items-center justify-center"
-            onClick={() => setIsNameClicked(!isNameClicked)}
+            onClick={() => setIsNameClicked((name) => !name)}
           >
             {userName}
+
             {isNameClicked && (
               <div className="bg-[#fff] absolute left-0 right-0 top-[40px] text-emerald-500 rounded-b-lg rounded-l-lg gap-y-[5px] flex flex-col font-normal px-[3px] py-[15px] text-[16px]">
                 <div className="flex items-center gap-x-[5px] ml-[5px]">
                   <img className="w-[20px]" src={UserIcon} alt="" />
                   <p>{userName}</p>
                 </div>
+
+                <div
+                  className="flex items-center hover:font-bold cursor-pointer gap-x-[5px] ml-[5px]"
+                  onClick={handleLogout}
+                >
+                  <img className="w-[20px]" src={LogoutIcon} alt="" />
+                  <p>Log Out</p>
+                </div>
               </div>
             )}
-
-          </div>
-          <div
-            className="flex items-center hover:font-bold cursor-pointer gap-x-[5px] ml-[5px]"
-            onClick={handleLogout}
-          >
-            <img className="w-[20px]" src={LogoutIcon} alt="" />
-            <p>Log Out</p>
           </div>
         </div>
-
       ) : (
-        <div className="flex items-center md:hidden">
-          <div onClick={toggleMenu} className="w-[40px] ml-[10px]">
-            {isMenuOpen ? (
-              <img src={CloseMenuIcon} alt="" className="cursor-pointer" />
-            ) : (
-              <img src={MenuIcon} alt="" className="cursor-pointer" />
-            )}
-          </div>
-
-        </div>
+        ""
       )}
+
+      <div className="flex items-center md:hidden">
+        <div onClick={toggleMenu} className="w-[40px] ml-[10px] md:ml-[20px]">
+          {isMenuOpen ? (
+            <img src={CloseMenuIcon} alt="" className="cursor-pointer" />
+          ) : (
+            <img src={MenuIcon} alt="" className="cursor-pointer" />
+          )}
+        </div>
+      </div>
     </header>
   );
 };
